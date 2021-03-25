0aaaa3d4ce60909c3e7033a7ea61f77110cdff8d
bewwari24 <77846729+bewwari24@users.noreply.github.com>
2021-03-26 01:30:33 +0700

    bbm

---
+++ QuizAppgames/app/src/main/java/com/example/quizappgames/Constants.kt
@@ -0,0 +1,13 @@
+import com.example.quizappgames.Quiztion
+
+object Constants{
+
+    fun getQuestions(): ArrayList<Quiztion> {
+        val questionsList = ArrayList<Quiztion>()
+
+        val que1 = Quiztion(1, "What country does this flag belong to?"
+
+        )
+    }
+}
+
